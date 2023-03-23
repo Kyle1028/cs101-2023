b7961c8a6343663cd418d565a26ea5eef1631ce2
Kyle1028 <93895301+Kyle1028@users.noreply.github.com>
2023-03-23 14:09:59 +0800

    Update main.c

--- Lab#4/main.c
+++ Lab#4/main.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+void replace_all(char *str, char oldChar, char newChar){
+    int len=0;
+    while(str[len]!= '\0'){len++;}
+    for(int i=0;i<len;i++){str[i]=(str[i]==oldChar)?newChar:oldChar;}
+}
+
+int main()
+{
+    char str[100], oldChar = 'a', newChar ='A';
+    printf("Enter any string: ");
+    scanf("%s", str);
+    printf("\nString before replacing: \n%s", str);
+    replace_all(str, oldChar, newChar);
+    printf("\n\n string after replacing %c with %c : \n%s", oldChar, newChar, str);
+    return 0;
+}
+#include <stdio.h>
+
+void replace_all(char *str, char oldChar, char newChar){
+    int len=0;
+    while(str[len]!= '\0'){len++;}
+    for(int i=0;i<len;i++){str[i]=(str[i]==oldChar)?newChar:oldChar;}
+}
+
+int main()
+{
+    char str[100], oldChar = 'a', newChar ='A';
+    printf("Enter any string: ");
+    scanf("%s", str);
+    printf("\nString before replacing: \n%s", str);
+    replace_all(str, oldChar, newChar);
+    printf("\n\n string after replacing %c with %c : \n%s", oldChar, newChar, str);
+    return 0;
+}
